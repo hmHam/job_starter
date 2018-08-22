fbeab6d1fe469333b54a94048d6e3b6fd749b22e
s-kato <s-kato@osaka-univ.coop>
2018-08-22 11:49:56 +0900

    #prepare.py 文字列がタプルになってた

--- prepare.py
+++ prepare.py
@@ -6,46 +6,53 @@ args = sys.argv
 
 try:
     project_name = args[1]
+    print(project_name)
 except IndexError:
     raise Exception('プロジェクト名を選んでください')
     exit()
 
+BACKEND_PATH = 'workspace/' + project_name + '_backend/src'
+FRONTEND_PATH = 'workspace/' + project_name + '_frontend/' + project_name
+
+path_list = [BACKEND_PATH, FRONTEND_PATH]
+for i in path_list:
+    os.system("code -n -a {0}".format(i))
+
 path_info = [
     {
-     'path': 'workspace/' + project_name + '_frontend/' + project_name,
+     'path': FRONTEND_PATH,
      'tab_name': 'ng build',
      'command': 'ng build --watch'
     },
     {
-     'path': 'workspace/' + project_name + '_frontend/' + project_name,
+     'path': FRONTEND_PATH,
      'tab_name': 'frontend',
      'command': 'git branch'
     },
     {
-     'path': 'workspace/' + project_name + '_backend/src',
+     'path': BACKEND_PATH,
      'tab_name': 'runserver',
      'command': 'python manage.py runserver'
     },
     {
-     'path': 'workspace/' + project_name + '_backend/src',
+     'path': BACKEND_PATH,
      'tab_name': 'backend',
      'command': 'git branch'
     },
     {
-     'path': 'workspace/' + project_name + '_backend/src',
+     'path': BACKEND_PATH,
      'tab_name': 'shell',
      'command': 'python manage.py shell'
     },
 ]
 
 for item in path_info:
-    os.system("guake --new-tab={path}".format(path=item['path']))
+    os.system(
+        "guake --new-tab={path}".format(path=item['path']))
     i = subprocess.getoutput("guake -g")
     os.system(
         "guake -i {index} --rename-tab='{tab_name}'".format(
-            index=i, tab_name=item['tab_name'])
-        )
+            index=i, tab_name=item['tab_name']))
     os.system(
         "guake -i {index} --execute-command='{command}'".format(
-            index=i, command=item['command'])
-        )
+            index=i, command=item['command']))
